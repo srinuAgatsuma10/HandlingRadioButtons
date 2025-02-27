1bf70649f160b41f034a870aee6d715072ca37a0
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-02-27 09:38:22 +0530

    Updated Code

--- src/test/java/DemoQA/RadioBtnDemoQa.java
+++ src/test/java/DemoQA/RadioBtnDemoQa.java
@@ -4,54 +4,54 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class RadioBtnDemoQa {
-	public static void main(String[] args) throws Exception{
-		
-		// Initiate WebDriver
-		WebDriver driver = new ChromeDriver();
+
+	public static WebDriver driver;
+	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+	@BeforeClass
+	public void setUp() {
+		driver = new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://demoqa.com/radio-button");
 		driver.manage().window().maximize();
-		
-		clickYesBtn(driver);
-		Thread.sleep(3000);
-		clickImpressiveBtn(driver);
-		Thread.sleep(3000);
-		clickNoBtn(driver);
-		
-		Assert.assertTrue(clickYesBtn(driver) && clickImpressiveBtn(driver) && clickNoBtn(driver), "All Checks Boxes are validated");
-		
-		driver.quit();		
+
 	}
-	
-	public static Boolean clickYesBtn(WebDriver driver) {
+
+	@Test(priority = 1)
+	public void clickYesBtn() {
 		driver.findElement(By.xpath("//label[normalize-space()='Yes']")).click();
 		String text = driver.findElement(By.xpath("//span[@class='text-success']")).getText();
-		if(text == "Yes") {
-			return true;
-		}
-		return false;
+
+		Assert.assertEquals(text, "Yes");
 	}
-	
-	public static Boolean clickImpressiveBtn(WebDriver driver) {
+
+	@Test(priority = 2)
+	public void clickImpressiveBtn() {
 		driver.findElement(By.xpath("//label[normalize-space()='Impressive']")).click();
 		String text = driver.findElement(By.xpath("//span[@class='text-success']")).getText();
-		if(text == "Impressive") {
-			return true;
-		}
-		return false;
+
+		Assert.assertEquals(text, "Impressive");
 	}
-	
-	public static boolean clickNoBtn(WebDriver driver) {
-		WebElement noBtn = driver.findElement(By.xpath("//label[normalize-space()='No']"));
-		if(noBtn.isEnabled()) {
-			noBtn.click();
-		}
-		return false;
+
+	@Test(priority = 3)
+	public void clickNoBtn() {
+		String noBtn = driver.findElement(By.xpath("//label[normalize-space()='No']")).getText();
+		System.out.println(noBtn);
+		Assert.assertTrue(true, "The Element is Not Clickable");
+
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
 	}
 }
