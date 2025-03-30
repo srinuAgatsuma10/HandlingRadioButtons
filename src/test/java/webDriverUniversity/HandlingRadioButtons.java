44391db9c2c385eb57572ebfcc775195865a8daf
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-03-30 19:55:32 +0530

    WebDriver University Update Commit

--- src/test/java/webDriverUniversity/HandlingRadioButtons.java
+++ src/test/java/webDriverUniversity/HandlingRadioButtons.java
@@ -1,8 +1,11 @@
 package webDriverUniversity;
 
 import java.time.Duration;
+import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -15,23 +18,42 @@ import org.testng.annotations.Test;
  */
 
 public class HandlingRadioButtons {
-	
+
 	WebDriver driver;
-	
+
 	@BeforeClass
 	public void setUp() {
 		driver = new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html");
-		driver.manage().window();
+		driver.manage().window().maximize();
 	}
-	
+
 	@Test
 	public void radioButtons() {
-		
+		// 1) Select custom radio button.
+		driver.findElement(By.xpath("//input[@value='green']")).click();
+
+		// 2) Select radio button which color starts with 'O'.
+		List<WebElement> radioButtons = driver
+				.findElements(By.xpath("//form[@id='radio-buttons']//input[@type='radio']"));
+		for (WebElement rb : radioButtons) {
+			String color = rb.getAttribute("value");
+			if (color.startsWith("o")) {
+				rb.click();
+			}
+		}
+
+		// 3) Check all buttons are Enabled, if enable Click one by one.
+		for (WebElement rb : radioButtons) {
+			if (rb.isEnabled()) {
+				rb.click();
+			}
+		}
+
 	}
-	
+
 	@AfterClass
 	public void tearDown() {
 		driver.quit();
